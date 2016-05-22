93c18bee8d35213b554b61ea24815901b2225374
Konstantin Kritsin <konst.kric@gmail.com>
2016-05-23 01:30:21 +0600

    TheParanoidAndroid

---
+++ HeatDetector.fs
@@ -0,0 +1,42 @@
+﻿module HeatDetector
+
+open System
+let R() = Console.In.ReadLine()
+(* W: width of the building. *)
+(* H: height of the building. *)
+let token = R().Split [|' '|]
+let W = int(token.[0])
+let H = int(token.[1])
+let N = int(R()) (* maximum number of turns before game over. *)
+let token1 = R().Split [|' '|]
+let mutable X0 = int(token1.[0])
+let mutable Y0 = int(token1.[1])
+let direction = [|"D"; "R"|]
+let prevCoord = [|0; 0|]
+let border = [|0; 0; W; H|]
+
+(* game loop *)
+while true do
+    let bombDir = Console.In.ReadLine().ToUpper() (* the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL) *)
+    if bombDir.Contains("U") then
+        Y0 <- Y0 - int(Math.Round(float(Y0 - border.[1])/float(2), MidpointRounding.AwayFromZero))
+        if prevCoord.[1] <> 0 then border.[3] <- prevCoord.[1]
+        direction.[0] <- "U"
+        prevCoord.[1] <- Y0
+    if bombDir.Contains("D") then
+        Y0 <- Y0 + int(Math.Round(float(border.[3] - 1 - Y0)/float(2), MidpointRounding.AwayFromZero))
+        if prevCoord.[1] <> 0 then border.[1] <- prevCoord.[1]
+        direction.[0] <- "D"
+        prevCoord.[1] <- Y0
+    if bombDir.Contains("L") then
+        X0 <- X0 - int(Math.Round(float(X0 - border.[0])/float(2), MidpointRounding.AwayFromZero))
+        if prevCoord.[0] <> 0 then border.[2] <- prevCoord.[0]
+        direction.[1] <- "L"
+        prevCoord.[0] <- X0
+    if bombDir.Contains("R") then 
+        X0 <- X0 + int(Math.Round(float(border.[2] - 1 - X0)/float(2), MidpointRounding.AwayFromZero))
+        if prevCoord.[0] <> 0 then border.[0] <- prevCoord.[0]
+        direction.[1] <- "R"
+        prevCoord.[0] <- X0
+    printfn "%d %d" X0 Y0
+    ()
---
+++ TheParanoidAndroid.fs
@@ -0,0 +1,53 @@
+﻿module TheParanoidAndroid
+
+(* Auto-generated code below aims at helping you parse *)
+(* the standard input according to the problem statement. *)
+open System
+
+(* nbFloors: number of floors *)
+(* width: width of the area *)
+(* nbRounds: maximum number of rounds *)
+(* exitFloor: floor on which the exit is found *)
+(* exitPos: position of the exit on its floor *)
+(* nbTotalClones: number of generated clones *)
+(* nbAdditionalElevators: ignore (always zero) *)
+(* nbElevators: number of elevators *)
+let token = (Console.In.ReadLine()).Split [|' '|]
+let nbFloors = int(token.[0])
+let width = int(token.[1])
+let nbRounds = int(token.[2])
+let exitFloor = int(token.[3])
+let exitPos = int(token.[4])
+let nbTotalClones = int(token.[5])
+let nbAdditionalElevators = int(token.[6])
+let nbElevators = int(token.[7])
+(* elevatorFloor: floor on which this elevator is found *)
+(* elevatorPos: position of the elevator on its floor *)
+let elevatorCoords = [|for i in 0 .. nbElevators - 1 do
+                        let token1 = (Console.In.ReadLine()).Split [|' '|]
+                        yield (int(token1.[0]), int(token1.[1]))|]
+
+
+(* game loop *)
+while true do
+    (* cloneFloor: floor of the leading clone *)
+    (* clonePos: position of the leading clone on its floor *)
+    (* direction: direction of the leading clone: LEFT or RIGHT *)
+    let token2 = (Console.In.ReadLine()).Split [|' '|]
+    let cloneFloor = int(token2.[0])
+    let clonePos = int(token2.[1])
+    let direction = token2.[2]
+
+    let neededDirection = 
+        match cloneFloor with
+        | -1 -> direction
+        | floor when floor = exitFloor -> if clonePos < exitPos then "RIGHT" else "LEFT"
+        | _ -> 
+            let elevPos = elevatorCoords |> Seq.filter(function
+                        | (elFloor, pos) when elFloor = cloneFloor -> true
+                        | _ -> false) |> Seq.head |> snd
+            if clonePos < elevPos then "RIGHT" else if clonePos > elevPos then "LEFT" else direction
+
+    if direction <> neededDirection then printfn "BLOCK"
+    else printfn "WAIT" (* action: WAIT or BLOCK *)
+    ()
